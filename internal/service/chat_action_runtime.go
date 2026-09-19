07a13e3f3a30ab020d37f919926165160f155d04
openvibely <openvibely@gmail.com>
2026-09-18 22:11:32 -0400

    Prepare changes for merge

--- internal/service/chat_action_runtime.go
+++ internal/service/chat_action_runtime.go
@@ -1684,8 +1684,8 @@ func applyProjectWorkerLimitUpdate(project *models.Project, req UpdateProjectSet
 		if req.MaxWorkers != nil {
 			return false, false, "clear_max_workers cannot be combined with max_workers"
 		}
-		changed = project.MaxWorkers != nil
-		shouldDispatch = isProjectWorkerLimitIncrease(project.MaxWorkers, nil)
+		changed = !models.ProjectWorkerLimitsEqual(project.MaxWorkers, nil)
+		shouldDispatch = models.ProjectWorkerLimitIncrease(project.MaxWorkers, nil)
 		project.MaxWorkers = nil
 		return changed, shouldDispatch, ""
 	}
@@ -1706,30 +1706,10 @@ func applyProjectWorkerLimitUpdate(project *models.Project, req UpdateProjectSet
 		v := maxWorkers
 		next = &v
 	}
-	changed = !sameProjectWorkerLimit(project.MaxWorkers, next)
-	shouldDispatch = isProjectWorkerLimitIncrease(project.MaxWorkers, next)
+	changed = !models.ProjectWorkerLimitsEqual(project.MaxWorkers, next)
+	shouldDispatch = models.ProjectWorkerLimitIncrease(project.MaxWorkers, next)
 	project.MaxWorkers = next
 	return changed, shouldDispatch, ""
-}
-
-func normalizedProjectWorkerLimit(limit *int) int {
-	if limit == nil || *limit <= 0 {
-		return 0
-	}
-	return *limit
-}
-
-func sameProjectWorkerLimit(a, b *int) bool {
-	return normalizedProjectWorkerLimit(a) == normalizedProjectWorkerLimit(b)
-}
-
-func isProjectWorkerLimitIncrease(oldLimit, newLimit *int) bool {
-	oldValue := normalizedProjectWorkerLimit(oldLimit)
-	newValue := normalizedProjectWorkerLimit(newLimit)
-	if oldValue == 0 {
-		return false
-	}
-	return newValue == 0 || newValue > oldValue
 }
 
 func projectDefaultModelSummary(model *models.LLMConfig) updateProjectSettingsModelSummary {
@@ -1746,7 +1726,7 @@ func projectDefaultModelSummary(model *models.LLMConfig) updateProjectSettingsMo
 }
 
 func projectWorkerLimitSummary(maxWorkers *int) updateProjectSettingsWorkerLimit {
-	value := normalizedProjectWorkerLimit(maxWorkers)
+	value := models.NormalizeProjectWorkerLimit(maxWorkers)
 	if value == 0 {
 		return updateProjectSettingsWorkerLimit{Set: false}
 	}
